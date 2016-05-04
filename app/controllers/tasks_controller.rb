1a992d09a89d2d21f39064fb044c5e787bbe21ad
Charles Pernet <charlespernet@gmail.com>
2016-05-04 13:39:56 +0200

    first functionnal ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,32 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
-    @task = Task.new
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @tasks = Task.all
+    @task = Task.create(task_params)
   end
 
   def edit
+    @tasks = Task.all
+    @task = Task.find(params[:id])
+    @task.update(done: true)
   end
 
   def update
+    @tasks = Task.all
+    @task = Task.find(params[:id])
+    @task.update(done: true)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
   end
 end
